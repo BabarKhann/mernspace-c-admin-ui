9894f7c145a783d7b8aee81d7dc36fa68c61d783
BabarKhann <babarkhan138@gmail.com>
2024-06-22 17:38:54 +0500

    add router

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,6 +1,4 @@
-import "./App.css";
-
-function App() {
+function HomePage() {
   return (
     <>
       <h1>Mernspace</h1>
@@ -8,4 +6,4 @@ function App() {
   );
 }
 
-export default App;
+export default HomePage;
